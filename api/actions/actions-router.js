b9d00ab4a72dfbe19e0880fa042274abed2878e0
MurrayWarnock <murray.warnock@gmail.com>
2021-08-05 17:11:31 -0600

    Actions and Projects router scaffolding

--- api/actions/actions-router.js
+++ api/actions/actions-router.js
@@ -1,8 +1,40 @@
 // Write your "actions" router here!
 const express = require('express');
 
-const Projects = require('./actions-model');
+const Actions = require('./actions-model');
 
 const router = express.Router();
+
+// module.exports = {
+//     get,
+//     insert,
+//     update,
+//     remove,
+//   };
+
+router.get('/actions', (req, res, next) => {
+    // Returns an array of actions (or an empty array) as the body of the response.
+});
+
+router.get('/actions/:id', (req, res, next) => {
+    // Returns an action with the given id as the body of the response.
+    // If there is no action with the given id it responds with a status code 404.
+});
+router.post('/actions', (req, res, next) => {
+    // Returns the newly created action as the body of the response.
+    // If the request body is missing any of the required fields it responds with a status code 400.
+    // When adding an action make sure the project_id provided belongs to an existing project
+});
+
+router.put('/actions/:id', (req, res, next) => {
+    // Returns the updated action as the body of the response.
+    // If there is no action with the given id it responds with a status code 404.
+    // If the request body is missing any of the required fields it responds with a status code 400.
+});
+
+router.delete('/actions/:id', (req, res, next) => {
+    // Returns no response body.
+    // If there is no action with the given id it responds with a status code 404.
+});
 
 module.exports = router;
--- api/projects/projects-router.js
+++ api/projects/projects-router.js
@@ -5,4 +5,43 @@ const Projects = require('./projects-model');
 
 const router = express.Router();
 
+// module.exports = {
+//     get,
+//     insert,
+//     update,
+//     remove,
+//     getProjectActions,
+//   };
+
+router.get('/projects', (req, res, next) => {
+    // Returns an array of projects as the body of the response.
+    // If there are no projects it responds with an empty array.
+});
+
+router.get('/projects/:id', (req, res, next) => {
+    // Returns a project with the given id as the body of the response.
+    // If there is no project with the given id it responds with a status code 404.
+});
+
+router.post('/projects', (req, res, next) => {
+    // Returns the newly created project as the body of the response.
+    // If the request body is missing any of the required fields it responds with a status code 400.
+});
+
+router.put('/projects/:id', (req, res, next) => {
+    // Returns the updated project as the body of the response.
+    // If there is no project with the given id it responds with a status code 404.
+    // If the request body is missing any of the required fields it responds with a status code 400.
+ });
+ 
+router.delete('/projects/:id', (req, res, next) => {
+     // RReturns no response body.
+     // If there is no project with the given id it responds with a status code 404.
+});
+
+router.get('/projects/:id/actions', (req, res, next) => {
+    // Returns an array of actions (could be empty) belonging to a project with the given id.
+    // If there is no project with the given id it responds with a status code 404.
+});
+
 module.exports = router;
