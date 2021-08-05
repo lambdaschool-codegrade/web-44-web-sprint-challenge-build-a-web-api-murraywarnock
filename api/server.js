e26ebe45c48f46d8e39913271e90befcc7e1d0d7
MurrayWarnock <murray.warnock@gmail.com>
2021-08-05 16:27:45 -0600

    Server running

--- api/server.js
+++ api/server.js
@@ -1,9 +1,19 @@
-const express = require('express');
-const server = express();
-
 // Configure your server here
 // Build your actions router in /api/actions/actions-router.js
 // Build your projects router in /api/projects/projects-router.js
 // Do NOT `server.listen()` inside this file!
+const express = require('express');
+
+const helmet = require('helmet');
+const projectsRouter = require('./projects/projects-router');
+const actionsRouter = require('./actions/actions-router');
+const server = express();
+
+server.use(express.json());
+server.use(helmet()); 
+// server.use('/api/projects', (req, res, next) => {next()}, projectsRouter);
+// server.use('/api/actions', (req, res, next) => {next()}, actionsRouter);
+
+
 
 module.exports = server;
--- index.js
+++ index.js
@@ -12,3 +12,12 @@ I need this code, but don't know where, perhaps should make some middleware, don
 
 Pull your server into this file and start it!
 */
+require('dotenv').config(); 
+
+const { PORT } = require('./config');
+const server = require('./api/server');
+
+server.listen(PORT, () => { 
+    console.log(`listening on ${PORT}`)
+});
+
