d6136db032e6590490ddc95875f72f5727abd5a5
MurrayWarnock <murray.warnock@gmail.com>
2021-08-06 17:09:57 -0600

    Final cleanup

--- api/server.js
+++ api/server.js
@@ -3,13 +3,21 @@ const express = require('express');
 
 const helmet = require('helmet');
 const apiRoutes = require("./apiRoutes");
-// const projectsRouter = require('./projects/projects-router');
-// const actionsRouter = require('./actions/actions-router');
 const server = express();
 
 server.use(express.json());
 server.use(helmet()); 
 server.use("/api", apiRoutes);
+server.get('/', (req, res, next) => {
+  res.send(`
+    <h1>Sprint 1 Challenge</h1>
+    <h2>Build a Web API</h2>
+    <h3>Web44, Unit 4, Sprint 1</h3>
+    <p>Murray Warnock</p>
+    <p>August 6, 2021</p>
+  `);
+  next();
+});
 
 server.use('*', (req, res, next) => {
     console.log(`hitting ${req.method} ${req.baseUrl}`);
